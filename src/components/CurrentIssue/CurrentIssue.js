de9c684070e0aab3843aaf180f0bfaa7c3e25cb2
KaayaRane <63211165+KaayaRane@users.noreply.github.com>
2022-06-09 12:02:49 -0500

    issue-tracker first commit

---
+++ src/components/CurrentIssue/CurrentIssue.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+import "./CurrentIssue.css";
+
+function CurrentIssue() {
+  const [addIssue, setAddIssue] = useState(true)
+
+
+  return (
+    <div className="current-issues">
+      <div className = "indv-issue">
+        <div>
+            <p className = "close-issue">Close Issue</p>
+            <p>Assigned<span>Username</span></p>
+            <p>Priority<span>High</span></p>
+            <p>Description</p>
+            <p className = "description-text">Thank you to everyone who reached to us to request teaching 
+                additional Juni subjects. We're excited to start connecting everyone with even more students!
+            </p>
+            <hr/>
+        </div>
+      </div>
+      <div className = "indv-issue">
+        <div>
+            <p className = "close-issue">Close Issue</p>
+            <p>Assigned<span>Username</span></p>
+            <p>Priority<span>High</span></p>
+            <p>Description</p>
+            <p className = "description-text">Thank you to everyone who reached to us to request teaching 
+                additional Juni subjects. We're excited to start connecting everyone with even more students!
+            </p>
+            <hr/>
+        </div>
+      </div>
+      <div className = "indv-issue">
+        <div>
+            <p className = "close-issue">Close Issue</p>
+            <p>Assigned<span>Username</span></p>
+            <p>Priority<span>High</span></p>
+            <p>Description</p>
+            <p className = "description-text">Thank you to everyone who reached to us to request teaching 
+                additional Juni subjects. We're excited to start connecting everyone with even more students!
+            </p>
+            <hr/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CurrentIssue;
