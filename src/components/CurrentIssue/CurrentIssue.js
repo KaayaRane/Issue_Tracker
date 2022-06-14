8d68b917759fd1fbc1bd08d78ef97dbdc4d168d4
KaayaRane <63211165+KaayaRane@users.noreply.github.com>
2022-06-14 11:09:26 -0500

    removed unused useEffect states

--- src/components/CurrentIssue/CurrentIssue.js
+++ src/components/CurrentIssue/CurrentIssue.js
@@ -1,8 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import "./CurrentIssue.css";
 
 function CurrentIssue() {
-  const [addIssue, setAddIssue] = useState(true)
 
 
   return (
