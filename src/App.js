8ed227c942e2005d23a196de5d7e4d18502e6106
KaayaRane <63211165+KaayaRane@users.noreply.github.com>
2022-06-16 17:52:42 -0500

    tried local storage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CurrentIssue from './components/CurrentIssue/CurrentIssue.js'
 import './App.css';
 
 function App() {
-  const [addIssue, setAddIssue] = useState(false)
+  const [addIssue, setAddIssue] = useState(true)
 
   function showAddIssue() {
     setAddIssue(true);
--- src/components/AddIssue/AddIssue.js
+++ src/components/AddIssue/AddIssue.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./AddIssue.css"
 
 function AddIssue() {
-
+  const [desc, setDesc] = useState(true)
+  const [dev, setDev] = useState(true)
+  const [priority, setPriority] = useState(true)
 
   return (
     <div className="add-issue">
       <form>
-          <label>Description<input type = "text" placeholder = "Description of Issue..."/></label>
+          <label>Description<input id = "description" type = "text" placeholder = "Description of Issue..." onKeyUp ={() => setDesc(document.getElementById("description").value)}/></label>
           <label>Assign To
-            <select name = "forDev" id = "forDev">
+            <select name = "forDev" id = "forDev" onChange = {() => setDev(document.getElementById("forDev").value)}>
                 <option value = "John">John</option>
                 <option value = "Sally">Sally</option>
                 <option value = "Barbara">Barbara</option>
@@ -17,14 +19,18 @@ function AddIssue() {
             </select>
           </label>
           <label>Priority Level
-            <select name = "priority" id = "priority">
+            <select name = "priority" id = "priority" onChange = {() => setPriority(document.getElementById("priority").value)}>
                 <option value = "Low">Low</option>
                 <option value = "Medium">Medium</option>
                 <option value = "High">High</option>
                 <option value = "Critical">Critical</option>
             </select>
           </label>
-          <button type = "submit">Add</button>
+          <button type = "submit" onSubmit = {(e) => {
+            e.preventDefault()
+            addToLocalStorage()
+
+          }}>Add</button>
       </form>
     </div>
   );
